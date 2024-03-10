7b8327f7e02fe5cedd8f83cb508cd661a820fe58
YuriiLeo <yuriileontenko@gmail.com>
2024-03-10 23:30:38 +0100

    fix: updated cors config from vercel

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,15 @@ const weatherRoutes = require("./src/routes/weatherRoutes");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors(
+    {
+        origin: ["weather-app-orcin-tau-56.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
+
 app.use(express.json());
 
 app.get("/api", (req, res) => {
